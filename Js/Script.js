96604d48f3a61fe357c82b71a369a8b558e5035c
1NonlyRohan <rohanjaiswal93774@gmail.com>
2025-01-17 16:20:09 +0530

    Submit: CodingNest Design Competition Website

--- Js/Script.js
+++ Js/Script.js
@@ -1,12 +1,12 @@
-// Smooth anchor tag...
+// Smooth Scrolling on anchor tags...
 document.addEventListener("DOMContentLoaded", function () {
-    const links = document.querySelectorAll('a[href^="#"]');
+    var links = document.querySelectorAll('a[href^="#"]');
 
     links.forEach(link => {
         link.addEventListener('click', function (event) {
             event.preventDefault();
-            const targetId = this.getAttribute('href').substring(1);
-            const targetElement = document.getElementById(targetId);
+            var targetId = this.getAttribute('href').substring(1);
+            var targetElement = document.getElementById(targetId);
 
             if (targetElement) {
                 targetElement.scrollIntoView({
@@ -19,3 +19,21 @@ document.addEventListener("DOMContentLoaded", function () {
 
 // AOS Animation Init...
 AOS.init();
+
+// Hamburger Menu bar...
+var hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+var mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+var menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+var header = document.querySelector('.header');
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
